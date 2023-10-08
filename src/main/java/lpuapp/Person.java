a861e787472c5cb52d5871d05f227642dba8c5f5
LiudmilaPlucci <efloorplan14@gmail.com>
2023-10-09 00:04:34 +0100

    added DB

--- src/main/java/lpuapp/Person.java
+++ src/main/java/lpuapp/Person.java
@@ -5,33 +5,74 @@ public class Person {
     private String lastName;
     private String userName;
     private String password;
+    private int tblPersonId;
     private static int userNameId = 103;
+    private static int idPerson = 1;
 
-    public String getLastName() {
-        return lastName;
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = String.valueOf(userNameId);
+        generateUsername(firstName, lastName);
+        userNameId ++;
+        this.tblPersonId = idPerson;
+        idPerson ++;
     }
 
-    public String getUserName() {
-        return userName;
-    }
+    public Person() {};
 
-    public String getPassword() {
-        return password;
+    private void generateUsername(String firstName, String lastName) {
+        this.userName = firstName.trim().charAt(0) + lastName.trim() + userNameId;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    protected Person(String firstName, String lastName) {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = String.valueOf(userNameId);
-        generateUsername(firstName, lastName);
-        userNameId++;
     }
 
-    private void generateUsername(String firstName, String lastName) {
-        this.userName = firstName.trim().charAt(0) + lastName.trim() + userNameId;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTblPersonId() {
+        return tblPersonId;
+    }
+
+    public void setTblPersonId(int tblPersonId) {
+        this.tblPersonId = tblPersonId;
+    }
+
+    @Override
+    public String toString() {
+        return "Person {" +
+                "tblPersonId = " + getTblPersonId() +
+                ", firstName = '" + getFirstName() + "'" +
+                ", lastName = '" + getLastName() + "'" +
+                ", username = '" + getUserName() + "'" +
+                ", password = '" + getPassword() + "'" +
+                "}";
     }
 }
--- src/main/java/lpuapp/SignUp.java
+++ src/main/java/lpuapp/SignUp.java
@@ -1,17 +1,58 @@
 package lpuapp;
 
+import lpuapp.database.DBUtils;
+
 import java.util.List;
 import java.util.Scanner;
 
 public class SignUp {
     private void printWelcomeMessage() {
-        System.out.println("        Welcome to LPU!");
+        System.out.println("Welcome to LPU!");
         Admin.printAdmins();
-        System.out.println();;
+        System.out.println();
     }
+
     private void signUp() {
-        Admin.addAdmin(new Admin("Ivan", "Sidorov"));
+        Admin admin = new Admin("Ivan", "Sidorov");
+        Admin.addAdmin(admin);
+
+        System.out.println();
+        System.out.println("___________________________________________");
+        System.out.println("Local Admins list");
         Admin.printAdmins();
+
+        System.out.println();
+        System.out.println("___________________________________________");
+        System.out.println("DB Users list");
+        List<Person> dbPersons = DBUtils.getTablePersonData();
+        System.out.println(dbPersons);
+
+        System.out.println();
+        System.out.println("___________________________________________");
+        System.out.println("DB Admins list");
+        List<Admin> dbAdmins = DBUtils.getTableAdminData();
+        System.out.println(dbAdmins);
+
+        admin = new Admin("Anrey", "Sidorov");
+        Admin.addAdmin(admin);
+
+        System.out.println();
+        System.out.println("___________________________________________");
+        System.out.println("Local Admins list");
+        Admin.printAdmins();
+
+        System.out.println();
+        System.out.println("___________________________________________");
+        System.out.println("DB Users list");
+        dbPersons = DBUtils.getTablePersonData();
+        System.out.println(dbPersons);
+
+        System.out.println();
+        System.out.println("___________________________________________");
+        System.out.println("DB Admins list");
+        dbAdmins = DBUtils.getTableAdminData();
+        System.out.println(dbAdmins);
+
 
         System.out.println();
         System.out.println("Enter 'Q' for quit OR");
@@ -22,7 +63,7 @@ public class SignUp {
         System.out.print("Enter username: ");
         String input = in.nextLine();
 
-        if(input.equals("Q") || input.equals("q")) {
+        if (input.equals("Q") || input.equals("q")) {
             System.out.println("Goodbye");
             System.exit(0);
         }
@@ -31,7 +72,7 @@ public class SignUp {
 
         System.out.print("Enter password: ");
         input = in.nextLine();
-        if(input.equals("Q") || input.equals("q")) {
+        if (input.equals("Q") || input.equals("q")) {
             System.out.println("Goodbye");
             System.exit(0);
         }
@@ -43,14 +84,15 @@ public class SignUp {
 
     private void checkCredentials(String userName, String password) {
         List<Admin> adminsList = Admin.getAdmins();
-        for(Admin admin: adminsList) {
-            if(admin.getUserName().equals(userName) && admin.getPassword().equals(password)) {
+        for (Admin admin : adminsList) {
+            if (admin.getUserName().equals(userName) && admin.getPassword().equals(password)) {
                 System.out.println("Welcome, " + admin.getFirstName() + " " + admin.getLastName() + "!");
-                Admin.runAdmin();
+                admin.runAdmin();
             } else {
                 System.out.println("Sorry, we can't recognize you. Check your credentials and try again later.");
                 System.out.println("Goodbye");
                 System.exit(0);
+                return;
             }
         }
     }
@@ -58,5 +100,6 @@ public class SignUp {
     public void runLPUApp() {
         printWelcomeMessage();
         signUp();
+
     }
 }
